4337849ad45b6ad9be868f836b59aba040864bd8
ursu96 <ursualexandru96@gmail.com>
2019-10-08 20:31:06 +0300

    first commit

---
+++ Istrate/Istrate/ConvertorPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Istrate
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConvertorPage : ContentPage
+	{
+		public ConvertorPage()
+		{
+			InitializeComponent ();
+            List<string> valute = new List<string>();
+            valute.Add("EUR");
+            valute.Add("USD");
+            valute.Add("RON");
+
+            pickerValutaSursa.ItemsSource = valute;
+            pickerValutaDestinatie.ItemsSource = valute;
+		}
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonConverteste_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Istrate/Istrate/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Istrate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertorToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConvertorPage());
+        }
+
+        private void DespreToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DesprePage());
+
+        }
+
+        private void FavoriteToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FavoritePage());
+
+        }
+
+        private void IstoricToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new IstoricPage());
+
+        }
+    }
+}
